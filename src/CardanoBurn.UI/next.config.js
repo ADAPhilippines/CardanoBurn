cd6e0330a3d2fa57a5275674014b0de5d7d4aae9
Clark Alesna <clark_alesna@hotmail.com>
2022-03-28 00:34:55 +0800

    Initial Code for CIP30 Integration and Burn tx construction

--- src/CardanoBurn.UI/next.config.js
+++ src/CardanoBurn.UI/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    CONTRACT_ADDRESS: 'addr_test1wrw77tq9hqe7yz2vls5xvdxvvnkvx70ppl5sun4h45kzzase2k5tj'
+  },
+  webpack: (config) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  }
 }
 
 module.exports = nextConfig
